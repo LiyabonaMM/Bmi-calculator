77d7ade9c802a00e494ff88a1f474cbd8650a1bf
Liyabona Mxhalisa <mxhalisalm@gmail.com>
2023-05-23 09:13:13 +0200

    Second Commit

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
-function calculateBMI() {
-    // Get input values
-    const weightInput = document.getElementById('weight');
-    const heightInput = document.getElementById('height');
-    const weight = parseFloat(weightInput.value);
-    const height = parseFloat(heightInput.value) / 100; // Convert height to meters
-  
-    // Calculate BMI
-    const bmi = weight / (height * height);
-  
-    // Display the result
-    const bmiResult = document.getElementById('bmi');
-    bmiResult.textContent = bmi.toFixed(2);
-    bmiResult.parentNode.style.display = 'block';
+function workoutBMI() {
+  // Get input values
+  const theWeightInputw = document.getElementById('weight');
+  const heightInputh = document.getElementById('height');
+  const weight = parseFloat(theWeightInputw.value);
+  const height = parseFloat(heightInputh.value) / 100; // Convert height to meters
+
+  // Calculate BMI
+  const bmi = weight / (height * height);
+
+  // Display the result
+  const myBmiResults = document.getElementById('bmi');
+  myBmiResults.textContent = bmi.toFixed(2);
+  myBmiResults.parentNode.style.display = 'block';
+
+  // Check overweight status
+  const theStatusResult = document.getElementById('status');
+  if (bmi >= 25) {
+    theStatusResult.textContent = 'Overweight';
+  } else {
+    theStatusResult.textContent = 'Not overweight';
   }
-  
+  theStatusResult.parentNode.style.display = 'block';
+}
